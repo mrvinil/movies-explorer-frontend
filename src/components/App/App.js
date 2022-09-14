2e491c77eff2bd32ce5feadb7f919ef078346e21
Vyacheslav K <i@kvs142.ru>
2022-09-14 13:17:39 +0300

    level-2: header and lead

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,21 @@
-import logo from '../../images/logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="container-fluid theme_dark_blue">
+        <header className="header">
+          <a href="/" className="header__logo link"></a>
+          <div className="header__personal">
+            <a href="#" className="header__personal-signup link">Регистрация</a>
+            <a href="#" className="header__personal-signin btn">Войти</a>
+          </div>
+        </header>
+        <section className="lead">
+          <h1 className="lead__title">Учебный проект студента факультета Веб-разработки.</h1>
+          <div className="lead__img"></div>
+        </section>
+      </div>
     </div>
   );
 }
