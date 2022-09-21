4bdbdfc7b827a503dc98e0f39fde978cf0ee083f
Vyacheslav K <i@kvs142.ru>
2022-09-21 15:01:43 +0300

    level-2: refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import Main from '../Main/Main';
-
 import './App.css';
-import Register from "../Register/Register";
-import Login from "../Login/Login";
-import Profile from "../Profile/Profile";
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import Profile from '../Profile/Profile';
 
 function App() {
   const [ isNavigationMenuOpen, setIsNavigationMenuOpen ] = React.useState(false);
   const [ isNavigationButtonClass, setIsNavigationButtonClass ] = React.useState('');
   const isOpen = isNavigationMenuOpen || isNavigationButtonClass;
 
+  // закрытие попапа по кнопке Esc
   React.useEffect(() => {
     function closeByEscape(evt) {
       if(evt.key === 'Escape') {
@@ -27,6 +26,7 @@ function App() {
     }
   }, [isOpen])
 
+  // открытие мобильного меню и изменение бургер кнопки
   function handleOpenNavigationMenu(){
     if (isNavigationMenuOpen) {
       setIsNavigationButtonClass('');
@@ -36,29 +36,32 @@ function App() {
     setIsNavigationMenuOpen(!isNavigationMenuOpen);
   }
 
+  // функция закрытия попапа
   function closeAllPopups() {
     setIsNavigationMenuOpen(false);
     setIsNavigationButtonClass('');
   }
 
-
   return (
     <>
-
       <main>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/signin" element={<Login />} />
-          <Route path="/profile" element={<Profile
-            isNavigationMenuOpen={isNavigationMenuOpen}
-            isNavigationButtonClass={isNavigationButtonClass}
-            handleOpenNavigationMenu={handleOpenNavigationMenu}
-            onClose={closeAllPopups}
-          />} />
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                isNavigationMenuOpen={isNavigationMenuOpen}
+                isNavigationButtonClass={isNavigationButtonClass}
+                handleOpenNavigationMenu={handleOpenNavigationMenu}
+                onClose={closeAllPopups}
+              />
+            }
+          />
         </Routes>
       </main>
-
     </>
   );
 }
