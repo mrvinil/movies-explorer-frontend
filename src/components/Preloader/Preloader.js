3d70978958eed03f6030c896236ab7a807a1f10a
Vyacheslav K <i@kvs142.ru>
2022-10-08 00:13:13 +0300

    level-3: fix навигации и прелоадера

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -2,17 +2,17 @@ import React from 'react'
 import './Preloader.css'
 
 const Preloader = ({ isOpen }) => {
-  return (
-    <>
-      {isOpen && (
-        <div className="preloader">
-          <div className="preloader__container">
-            <span className="preloader__round"></span>
+    return (
+      <>
+        {isOpen && (
+          <div className="preloader">
+            <div className="preloader__container">
+              <span className="preloader__round"></span>
+            </div>
           </div>
-        </div>
-      )}
-    </>
-  );
+        )}
+      </>
+    );
 };
 
 export default Preloader
